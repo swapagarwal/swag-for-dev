80c1a54fe313dae0a312ad2900f577130c0d0dcc
Vikas <potlurivikas@gmail.com>
2018-10-11 21:27:52 -0500

    cleanup + remove unneeded code

--- site/src/js/index.js
+++ site/src/js/index.js
@@ -14,12 +14,7 @@ const sort = {
 let contentEl = document.getElementById('content'),
     filterInput = document.getElementById('filter'),
     sortingInput = document.getElementById('sorting'),
-    firstLoad = true,
-    selector = new Selectr('#tags', {
-        multiple: true,
-        placeholder: 'Choose tags...',
-        data: window.swagTags.map(tag => ({value: tag, text: tag}))
-    });
+    firstLoad = true;
 
 function allowDifficultySelect(shouldAllow) {
     sortingInput.querySelectorAll('.difficulty')
@@ -49,57 +44,18 @@ function handleSort() {
         .forEach(sortedChild => contentEl.appendChild(sortedChild));
 }
 
-const renderSwag = () => {
-    UrlHandler();
-
-    contentEl.innerHTML = '';
-
-    const filter = getFilter();
-    const sorting = getSorting();
-    const tagSort = getTagValue();
-
-    window.swag
-        .filter(v => filter === 'All difficulties' ? true : v.difficulty === filter.toLowerCase())
-        .filter(v => tagSort.length ? tagSort.every(val => v.tags.includes(val)) : true)
-        .map(item => {
-            const {difficulty} = item;
-            contentEl.innerHTML += `
-                <div class='item'>
-                    <div class='title flex'>
-                        <h1>${item.name}</h1>
-                        <div class='difficulty ${difficulty}' title='${difficulty} difficulty'>
-                            <span class="sr-only">Difficulty: ${difficulty}</span>
-                        </div>
-                    </div>
-                    <p class='swag'>
-                        ${item.tags.map(tag => `<span>${tag}</span>`).join('')}
-                    </p>
-                    <div class='flex img-container'>
-                        <img src='${item.image}' alt="${item.name} swag you can get"></img>
-                    </div>
-                    <p class='description'>${item.description}</p>
-                    <a href='${item.reference}'>Check it out</a>
-                </div>
-            `;
-        });
-};
-
-const difficultyIndex = diff => ['easy', 'medium', 'hard'].indexOf(diff);
-
-const getFilter = () => filterInput.value;
-const getSorting = () => sortingInput.value;
-const getTagValue = () => selector.getValue();
+const getTagValue = () => window.selector.getValue();
 
 const UrlHandler = () => {
     if ('URLSearchParams' in window) {
+        const {selector} = window;
         let searchParams = new URLSearchParams(window.location.search);
         if (firstLoad) {
             firstLoad = false;
             if (searchParams.has('tags')) {
                 selector.setValue(searchParams.get('tags').split(' '));
-                renderSwag();
             }
-            selector.on('selectr.change', renderSwag);
+            // @todo selector.on('selectr.change', renderSwag);
         } else {
             if (getTagValue().length) {
                 searchParams.set('tags', getTagValue().join(' '));
@@ -113,8 +69,13 @@ const UrlHandler = () => {
 };
 
 window.addEventListener('load', () => {
-    UrlHandler();
+    window.selector = new Selectr('#tags', {
+        multiple: true,
+        placeholder: 'Choose tags...',
+        data: window.swagTags.map(tag => ({ value: tag, text: tag }))
+    });
 
+    UrlHandler();
     filterInput.addEventListener('input', handleFilter);
     sortingInput.addEventListener('input', handleSort);
 });
