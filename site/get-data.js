8988a9d7a0c8d46321932b47076a585e24790363
Vikas Potluri <vikaspotluri123.github@gmail.com>
2018-11-05 14:53:29 -0600

    :memo: Add todo comments

--- site/get-data.js
+++ site/get-data.js
@@ -1,6 +1,7 @@
 const swagImages = [];
 const fileNames = [];
 const swagList = require('../data.json').map(swag => {
+    // @todo: remove once `active: Boolean` is added to data.json
     if (swag.tags.includes('hacktoberfest') {
         return false;
     }
@@ -28,6 +29,7 @@ const swagList = require('../data.json').map(swag => {
     fileNames.push(fileName);
 
     return swag;
+// @todo: remove (if needed) once `active: Boolean` is added to data.json
 }).filter(Boolean);
 
 module.exports = { swagList, swagImages };
