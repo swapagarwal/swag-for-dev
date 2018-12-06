5adcbae6183459c6bae431a567c9e63a59d8aabf
Zadkiel <zadkiel.aharonian@gmail.com>
2018-12-06 02:57:48 +0100

    chore(lint): switch to XO (#189)
    * use xo instead of eslint
    
    * lint code

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,10 @@
  */
 const ACTIVE_CLASS = 'visible';
 const sort = {
-    ALPHABETICAL_ASCENDING: (a,b) => a.dataset.name > b.dataset.name ? 1 : -1,
-    ALPHABETICAL_DESCENDING: (a,b) => a.dataset.name < b.dataset.name ? 1 : -1,
-    DIFFICULTY_ASCENDING: (a,b) => a.dataset.difficulty > b.dataset.difficulty ? 1 : -1,
-    DIFFICULTY_DESCENDING: (a,b) => a.dataset.difficulty < b.dataset.difficulty ? 1 : -1
+	ALPHABETICAL_ASCENDING: (a, b) => a.dataset.name > b.dataset.name ? 1 : -1,
+	ALPHABETICAL_DESCENDING: (a, b) => a.dataset.name < b.dataset.name ? 1 : -1,
+	DIFFICULTY_ASCENDING: (a, b) => a.dataset.difficulty > b.dataset.difficulty ? 1 : -1,
+	DIFFICULTY_DESCENDING: (a, b) => a.dataset.difficulty < b.dataset.difficulty ? 1 : -1
 };
 
 const contentEl = document.getElementById('content');
@@ -17,87 +17,90 @@ const sortingInput = document.getElementById('sorting');
 
 const activateElements = els => Array.from(els).forEach(node => node.classList.add(ACTIVE_CLASS));
 const allowDifficultySelect = shouldAllow => sortingInput.querySelectorAll('.difficulty')
-    .forEach(node => node.disabled = !shouldAllow);
+	.forEach(node => {
+		node.disabled = !shouldAllow;
+	});
 
-let search, selectr;
+let search;
+let selectr;
 
 function handleDifficulty(difficultyChanged) {
-    const {value} = filterInput;
-    Array.from(contentEl.getElementsByClassName(ACTIVE_CLASS))
-        .forEach(swag => swag.classList.remove(ACTIVE_CLASS));
+	const {value} = filterInput;
+	Array.from(contentEl.getElementsByClassName(ACTIVE_CLASS))
+		.forEach(swag => swag.classList.remove(ACTIVE_CLASS));
 
-    if (value === 'alldifficulties') {
-        activateElements(contentEl.querySelectorAll('.item'));
-        allowDifficultySelect(true);
-    } else {
-        activateElements(contentEl.getElementsByClassName(value));
-        allowDifficultySelect(false);
-    }
+	if (value === 'alldifficulties') {
+		activateElements(contentEl.querySelectorAll('.item'));
+		allowDifficultySelect(true);
+	} else {
+		activateElements(contentEl.getElementsByClassName(value));
+		allowDifficultySelect(false);
+	}
 
-    if (difficultyChanged && sortingInput.selectedIndex > 1) {
-        sortingInput.selectedIndex = 0;
-        return true;
-    }
+	if (difficultyChanged && sortingInput.selectedIndex > 1) {
+		sortingInput.selectedIndex = 0;
+		return true;
+	}
 
-    return false;
+	return false;
 }
 
 function handleTags() {
-    const tags = selectr.getValue();
+	const tags = selectr.getValue();
 
-    if (!tags.length) {
-        history.pushState(null, '', window.location.pathname);
-        return;
-    }
+	if (tags.length === 0) {
+		history.pushState(null, '', window.location.pathname);
+		return;
+	}
 
-    Array.from(contentEl.querySelectorAll('.item')).forEach(el => {
-        const show = tags.reduce((sho, tag) => sho || el.classList.contains(`tag-${tag}`), false);
-        if (!show) {
-            el.classList.remove('visible');
-        }
-    });
+	Array.from(contentEl.querySelectorAll('.item')).forEach(el => {
+		const show = tags.reduce((sho, tag) => sho || el.classList.contains(`tag-${tag}`), false);
+		if (!show) {
+			el.classList.remove('visible');
+		}
+	});
 
-    if (!search) {
-        return;
-    }
-    search.set('tags', tags.join(' '));
-    const newRelativePathQuery = `${window.location.pathname}?${search.toString()}`;
-    history.pushState(null, '', newRelativePathQuery);
+	if (!search) {
+		return;
+	}
+	search.set('tags', tags.join(' '));
+	const newRelativePathQuery = `${window.location.pathname}?${search.toString()}`;
+	history.pushState(null, '', newRelativePathQuery);
 }
 
 function handleSort() {
-    Array.from(contentEl.children)
-        .map(child => contentEl.removeChild(child))
-        .sort(sort[sortingInput.value])
-        .forEach(sortedChild => contentEl.appendChild(sortedChild));
+	Array.from(contentEl.children)
+		.map(child => contentEl.removeChild(child))
+		.sort(sort[sortingInput.value])
+		.forEach(sortedChild => contentEl.appendChild(sortedChild));
 }
 
 // The cascade is the function which handles calling filtering and sorting swag
 function cascade(force = false) {
-    force |= handleDifficulty(this === filterInput);
-    force |= handleTags(Boolean(this.el));
-    if (force || this === sortingInput) {
-        handleSort(this === sortingInput);
-    }
+	force |= handleDifficulty(this === filterInput);
+	force |= handleTags(Boolean(this.el));
+	if (force || this === sortingInput) {
+		handleSort(this === sortingInput);
+	}
 }
 
 window.addEventListener('load', () => {
-    selectr = new Selectr('#tags', {
-        multiple: true,
-        placeholder: 'Choose tags...',
-        data: window.swagTags.map(tag => ({ value: tag, text: tag }))
-    });
+	selectr = new Selectr('#tags', {
+		multiple: true,
+		placeholder: 'Choose tags...',
+		data: window.swagTags.map(tag => ({value: tag, text: tag}))
+	});
 
-    if ('URLSearchParams' in window) {
-        search = new URLSearchParams(window.location.search);
-        if (search.has('tags')) {
-            selectr.setValue(search.get('tags').split(' '));
-        }
-    }
+	if ('URLSearchParams' in window) {
+		search = new URLSearchParams(window.location.search);
+		if (search.has('tags')) {
+			selectr.setValue(search.get('tags').split(' '));
+		}
+	}
 
-    selectr.on('selectr.change', cascade);
-    filterInput.addEventListener('input', cascade);
-    sortingInput.addEventListener('input', cascade);
+	selectr.on('selectr.change', cascade);
+	filterInput.addEventListener('input', cascade);
+	sortingInput.addEventListener('input', cascade);
 
-    cascade.call(window, true);
+	cascade.call(window, true);
 });
