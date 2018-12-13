f42e2c33d8371382daddcd30feeb68ecba898a00
Vikas Potluri <vikaspotluri123.github@gmail.com>
2018-12-13 12:38:46 -0600

    add individual tests for swag

--- test/functional/data.spec.js
+++ test/functional/data.spec.js
@@ -3,38 +3,39 @@ const {expect} = require('chai');
 
 let data;
 
+function checkURL(url, head = false) {
+	const method = head ? 'head' : 'get';
+	return got[method](url, {throwHttpErrors: false}).then(({statusCode}) => {
+		if (statusCode === 403) {
+			return checkURL(url, true);
+		}
+		expect(statusCode).to.equal(200);
+	});
+}
+
 describe('swag-for-dev', function () {
 	it('data.json is valid', function () {
 		data = require('../../data.json');
 		expect(data).to.be.an('Array');
 	});
 
-	it('images and references are valid', function () {
-		const promises = data.map(async ({image, reference}) => {
-			expect(image).to.be.ok;
-			expect(reference).to.be.ok;
+	describe('valid images and references', function () {
+		data = require('../../data.json');
+		delete require.cache[require.resolve('../../data.json')];
+		data.forEach(opportunity => {
+			describe(opportunity.name, function () {
+				it('valid reference', function () {
+					this.timeout(6500);
+					this.slow(1500);
+					return checkURL(opportunity.reference);
+				});
 
-			const imageValid = await got.head(image).catch(({response}) => {
-				if (response.statusCode === 403) {
-					return got(image);
-				}
+				it('valid image', function () {
+					this.timeout(6500);
+					this.slow(1500);
+					return checkURL(opportunity.image);
+				});
 			});
-
-			const referenceValid = await got.head(reference).catch(({response}) => {
-				if (response.statusCode === 403) {
-					return got(reference);
-				}
-			});
-
-			try {
-				expect(imageValid.statusCode).to.equal(200);
-				expect(referenceValid.statusCode).to.equal(200);
-			} catch (error) {
-				// @todo remove
-				console.log(error, image, reference);
-			}
 		});
-
-		return Promise.all(promises);
 	});
 });
