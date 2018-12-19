9ea04ea48dab25512537cb2b57732b9a8557a036
Zadkiel <zadkiel.aharonian@gmail.com>
2018-12-19 17:16:14 +0200

    Remove redundant verification
    Co-Authored-By: itaisteinherz <itaisteinherz@gmail.com>

--- src/js/index.js
+++ src/js/index.js
@@ -61,7 +61,7 @@ function handleTags() {
 		}
 	});
 
-	if (!search || !search.get('tags') || search.get('tags') === '') {
+	if (!search || !search.get('tags')) {
 		return;
 	}
 	search.set('tags', tags.join(' '));
