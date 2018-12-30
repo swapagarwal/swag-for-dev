495479b8db8e1d60d522eb6132af94224cb5347e
Zadkiel <hello@zadkiel.fr>
2018-12-30 17:18:49 +0000

    Make Selectr look like other select inputs

--- src/js/index.js
+++ src/js/index.js
@@ -14,6 +14,7 @@ const sort = {
 const contentEl = document.getElementById('content');
 const filterInput = document.getElementById('filter');
 const sortingInput = document.getElementById('sorting');
+const tagsSelect = document.getElementById('tags');
 
 const activateElements = els => Array.from(els).forEach(node => node.classList.add(ACTIVE_CLASS));
 const allowDifficultySelect = shouldAllow => sortingInput.querySelectorAll('.difficulty')
@@ -87,9 +88,11 @@ function cascade(force = false) {
 window.addEventListener('load', () => {
 	selectr = new Selectr('#tags', {
 		multiple: true,
+		searchable: false,
 		placeholder: 'Choose tags...',
 		data: window.swagTags.map(tag => ({value: tag, text: tag}))
 	});
+	selectr.on('selectr.init', () => tagsSelect.classList.remove('hidden'));
 
 	if ('URLSearchParams' in window) {
 		search = new URLSearchParams(window.location.search);
