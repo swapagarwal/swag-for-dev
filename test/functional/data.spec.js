f08ebb9d8f2edf47077aca8cd0f0e800fa07e946
Vikas <vikaspotluri123.github@gmail.com>
2019-01-25 18:47:47 -0600

    Add data object schema tests

--- test/functional/data.spec.js
+++ test/functional/data.spec.js
@@ -9,6 +9,7 @@ function checkURL(url, head = false) {
 		if (statusCode === 403) {
 			return checkURL(url, true);
 		}
+
 		expect(statusCode).to.equal(200);
 	});
 }
@@ -17,12 +18,21 @@ describe('swag-for-dev', function () {
 	it('data.json is valid', function () {
 		data = require('../../data.json');
 		expect(data).to.be.an('Array');
+		data.forEach(datum => {
+			expect(datum.name).to.be.a('string');
+			expect(datum.difficulty).to.be.oneOf(['easy', 'medium', 'hard']);
+			expect(datum.reference).to.be.a('string');
+			expect(datum.image).to.be.a('string');
+			expect(datum.dateAdded).to.match(/20\d\d-[0-1][\d]-[0-3][\d]T\d\d:\d\d:\d\d\.000Z/);
+			expect(datum.tags).to.be.an('Array').with.length.greaterThan(0);
+		});
 	});
 
 	describe('valid images and references', function () {
 		data = require('../../data.json');
 		delete require.cache[require.resolve('../../data.json')];
 		data.forEach(opportunity => {
+			/* eslint-disable max-nested-callbacks */
 			describe(opportunity.name, function () {
 				it('valid reference', function () {
 					this.timeout(6500);
@@ -36,6 +46,7 @@ describe('swag-for-dev', function () {
 					return checkURL(opportunity.image);
 				});
 			});
+			/* eslint-enable max-nested-callbacks */
 		});
 	});
 });
